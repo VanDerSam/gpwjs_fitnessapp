d5e112bd6f45138d4f84f94bae159cc69c2fbdf5
Sergey Muratov <van.der.msa@gmail.com>
2021-12-08 00:17:53 +0300

    Function "Log a session" implemented

--- js/controller.js
+++ js/controller.js
@@ -15,28 +15,51 @@
         loadUser(selectedUser);
     };
 
+    function getById (id) {
+        return document.getElementById(id);
+    };
+
+    var log = function (sessionDate, duration) {
+        var user = fitnessApp.userManager.getCurrentUser();
+        user.addSession(sessionDate, duration);
+        fitnessApp.userView.render(user);
+        return "Thanks for logging your session.";
+    };
+
+    var logSession = function () {
+        var sessionDateString = getById("sessionDate").value;
+        var sessionDate = Date.parse(sessionDateString);
+        var sessionDuration = parseInt(getById("sessionDuration").value);
+        fitnessApp.messageView.clear();
+        if (!sessionDate) {
+            fitnessApp.messageView.render("Wrong date");
+            return;
+        }
+        if (!sessionDuration) {
+            fitnessApp.messageView.render("Wrong duration. Should be an integer value");
+            return;
+        }
+        log(sessionDateString,sessionDuration);
+    };
+
     var init = function () {
       fitnessApp.userManager.loadUserList("users", function (userList) {
         if (userList === null || userList.length === 0) {
             return;
-        }  
-        // fitnessApp.userManager.loadUser(userList[0], function(user) {
-        //     fitnessApp.userView.render(user);
-        // });
+        }
+
         fitnessApp.userListView.render(userList);
         loadUser(userList[0]);
 
         var buttonLoadUser = document.getElementById("loadUser");
         buttonLoadUser.addEventListener("click", loadSelectedUser);
+
+        var buttonLogSession = document.getElementById("logSession");
+        buttonLogSession.addEventListener("click", logSession);
     });
       
       return {
-        log: function (sessionDate, duration) {
-          var user = fitnessApp.userManager.getCurrentUser();
-          user.addSession(sessionDate, duration);
-          fitnessApp.userView.render(user);
-          return "Thanks for logging your session.";
-        },
+        log: log,
         loadUser: loadUser
       };
     };
